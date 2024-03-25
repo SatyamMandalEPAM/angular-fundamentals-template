da4b54e35d59668104bc9ce59bdd2d4906e99f18
SatyamMandal <mandal0927@gmail.com>
2024-03-25 10:40:53 +0530

    Course form reactive validators.

--- src/app/shared/components/course-form/course-form.component.ts
+++ src/app/shared/components/course-form/course-form.component.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {
-  FormBuilder, FormGroup
+  FormBuilder, FormControl, FormGroup, Validators
 } from '@angular/forms';
 import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
 import { fas } from '@fortawesome/free-solid-svg-icons';
@@ -10,10 +10,22 @@ import { fas } from '@fortawesome/free-solid-svg-icons';
   templateUrl: './course-form.component.html',
   styleUrls: ['./course-form.component.scss'],
 })
-export class CourseFormComponent {
+export class CourseFormComponent implements OnInit {
   constructor(public fb: FormBuilder, public library: FaIconLibrary) {
     library.addIconPacks(fas);
   }
   courseForm!: FormGroup;
+
+  ngOnInit(): void {
+    this.courseForm = new FormGroup({
+      title : new FormControl('', [Validators.required, Validators.minLength(2)]),
+      description : new FormControl('', [Validators.required, Validators.minLength(2)]),
+      author : new FormControl('', [ ]),
+      duration : new FormControl('', [Validators.required, Validators.min(0)])
+    });
+  }
   // Use the names `title`, `description`, `author`, 'authors' (for authors list), `duration` for the form controls.
+  onSubmit() {
+    console.log(this.courseForm.value);
+  }
 }
