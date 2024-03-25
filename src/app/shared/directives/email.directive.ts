c4833b1a81334b940935a0b1925d3f0bbdf5b8f6
SatyamMandal <mandal0927@gmail.com>
2024-03-25 18:24:18 +0530

    Email directive added.

--- src/app/shared/directives/email.directive.ts
+++ src/app/shared/directives/email.directive.ts
@@ -1,9 +1,14 @@
-import { Directive } from "@angular/core";
+import { Directive, Input } from "@angular/core";
+import { NG_VALIDATORS, Validator, FormControl } from '@angular/forms';
 
 @Directive({
     selector: '[emailValidator]',
-    providers: [/*Add your code here*/]
+    providers: [{ provide: NG_VALIDATORS, useExisting: EmailValidatorDirective, multi: true }]
 })
-export class EmailValidatorDirective {
-    // Add your code here
+export class EmailValidatorDirective implements Validator {
+    validate(control: FormControl) {
+        const emailRegex = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/i;
+        const valid = emailRegex.test(control.value);
+        return valid ? null : { invalidEmail: true };
+    }
 }
