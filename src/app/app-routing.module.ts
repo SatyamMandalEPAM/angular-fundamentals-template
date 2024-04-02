3dafe3504e8c35aa7131ef77c9dc34812e2c4535
SatyamMandal <mandal0927@gmail.com>
2024-04-02 11:20:19 +0530

    updated route table

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { LoginFormComponent, RegistrationFormComponent } from './shared/components';
+import { CoursesListComponent } from './features/courses-list/courses-list.component';
 
 export const routes: Routes = [
-    { path: '/login', component: LoginFormComponent},
-    { path: '/registration', component: RegistrationFormComponent}
+    { path: 'login', component: LoginFormComponent},
+    { path: 'registration', component: RegistrationFormComponent},
+    { path: 'courses', component: CoursesListComponent}
 ];
