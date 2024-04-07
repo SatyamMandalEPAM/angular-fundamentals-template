6f83145e81f49d899224788dc1737c88e58a7977
SatyamMandal <mandal0927@gmail.com>
2024-04-07 17:38:50 +0530

    Create courses actions.

--- src/app/store/courses/courses.actions.ts
+++ src/app/store/courses/courses.actions.ts
@@ -1,9 +1,35 @@
 import { createAction, props } from '@ngrx/store';
 import { CoursesConstants } from '@app/store/courses/courses.constants';
 
-// Add your code here
+//Actions for request all Courses
 export const requestAllCourses = createAction(CoursesConstants.REQUEST_ALL_COURSES);
-
 export const requestAllCoursesSuccess = createAction(CoursesConstants.REQUEST_ALL_COURSES_SUCCESS, props<{courses : string}>());
-
 export const requestAllCoursesFail = createAction(CoursesConstants.REQUEST_ALL_COURSES_FAIL, props<{error : string}>());
+
+//Actions for request individual course
+export const requestSingleCourse = createAction(CoursesConstants.REQUEST_SINGLE_COURSE, props<{id : string}>());
+export const requestSingleCourseSuccess = createAction(CoursesConstants.REQUEST_SINGLE_COURSE_SUCCESS, props<{course : string}>());
+export const requestSingleCourseFail = createAction(CoursesConstants.REQUEST_SINGLE_COURSE_FAIL, props<{error : string}>());
+
+//Actions for request filtered Courses
+export const requestFilteredCourses = createAction(CoursesConstants.REQUEST_FILTERED_COURSES, props<{title : string}>());
+export const requestFilteredCoursesSuccess = createAction(CoursesConstants.REQUEST_FILTERED_COURSES_SUCCESS, props<{courses : string}>());
+export const requestFilteredCoursesFail = createAction(CoursesConstants.REQUEST_FILTERED_COURSES_FAIL, props<{error : string}>());
+
+//Actions for delete course
+export const requestDeleteCourse = createAction(CoursesConstants.REQUEST_DELETE_COURSE, props<{id : string}>());
+export const requestDeleteCourseSuccess = createAction(CoursesConstants.REQUEST_DELETE_COURSE_SUCCESS);
+export const requestDeleteCourseFail = createAction(CoursesConstants.REQUEST_DELETE_COURSE_FAIL, props<{error : string}>());
+
+//Actions for edit course
+export const requestEditCourse = createAction(CoursesConstants.REQUEST_EDIT_COURSE, props<{id : string, course : string}>());
+export const requestEditCourseSuccess = createAction(CoursesConstants.REQUEST_EDIT_COURSE_SUCCESS, props<{course : string}>());
+export const requestEditCourseFail = createAction(CoursesConstants.REQUEST_EDIT_COURSE_FAIL, props<{error : string}>());
+
+//Actions for create course
+export const requestCreateCourse = createAction(CoursesConstants.REQUEST_CREATE_COURSE, props<{course : string}>());
+export const requestCreateCourseSuccess = createAction(CoursesConstants.REQUEST_CREATE_COURSE_SUCCESS, props<{course : string}>());
+export const requestCreateCourseFail = createAction(CoursesConstants.REQUEST_EDIT_COURSE_FAIL, props<{error : string}>());
+
+
+
