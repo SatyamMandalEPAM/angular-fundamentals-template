dbec70f50ff70f1fb836b6551ee78385bec0a82e
SatyamMandal <mandal0927@gmail.com>
2024-04-07 17:19:25 +0530

    Actions for request all Courses.

--- src/app/store/courses/courses.actions.ts
+++ src/app/store/courses/courses.actions.ts
@@ -2,3 +2,8 @@ import { createAction, props } from '@ngrx/store';
 import { CoursesConstants } from '@app/store/courses/courses.constants';
 
 // Add your code here
+export const requestAllCourses = createAction(CoursesConstants.REQUEST_ALL_COURSES);
+
+export const requestAllCoursesSuccess = createAction(CoursesConstants.REQUEST_ALL_COURSES_SUCCESS, props<{courses : string}>());
+
+export const requestAllCoursesFail = createAction(CoursesConstants.REQUEST_ALL_COURSES_FAIL, props<{error : string}>());
